2b4ee1d63ce3d4fdd24f7fc0ecc04ed35ffcd04f
Marek Suchánek <marek.suchanek@gmail.com>
2025-02-20 08:27:38 +0100

    Release 0.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@ sys.path.insert(0, str(pathlib.Path(__file__).parent / '_ext'))
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'OSTrails'
-release = '0.1'
+release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
